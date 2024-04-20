52ce0dceb899ee75f3567fdb3b3d55a3dfe90ef3
PichiBeta <cab2209@gmail.com>
2024-04-20 14:01:34 -0400

    csv file in project

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by Christian Betancourt on 4/20/24.
 //
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main(){
+    return 0;
+    //TODO:
+    // create hash map to keep count of companies
+    // pass info from hash map into a vector in form of pairs
+    // create heap
+}
