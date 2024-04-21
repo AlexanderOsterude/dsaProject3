0e17da8ced04074b4cebcf9702f277de6105d792
PichiBeta <cab2209@gmail.com>
2024-04-20 22:27:46 -0400

    small changes

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,25 @@ using namespace std;
 
 int main(){
 
-    unordered_map<string, int> pairs = {
-            {"apple", 1},
-            {"banana", 10},
-            {"orange", 3},
-            {"weird fruit", -1111},
-            {"grape", 1000},
-            {"pineapple", 6}
-    };
-    vector<pair<string, int>> vec;
-    vec = heapSort(pairs);
-    for (auto p: vec){
-        cout << p.first << ": " << p.second << endl;
-    }
+//    unordered_map<string, int> pairs = {
+//            {"apple", 1},
+//            {"banana", 10},
+//            {"orange", 3},
+//            {"weird fruit", -1111},
+//            {"grape", 1000},
+//            {"pineapple", 6}
+//    };
+//    vector<pair<string, int>> vec;
+//    vec = heapSort(pairs);
+//    for (auto p: vec){
+//        cout << p.first << ": " << p.second << endl;
+//    }
     return 0;
     //TODO:
     // pass in info from csv file (Alex)
     // create hash map to keep count of companies and number of their occurrences (Alex)
-    // create sorting algorithm (Chris)
-    // create heapsort or mergesort (Chris)
+    // create sorting algorithm (Chris) DONE
+    // create heapsort or mergesort (Chris) DONE
     // once this is done, we should create a more intricate main file for user to make decisions
     //  -Number of companies to display
     //  -Whether they want to display top or bottom companies
